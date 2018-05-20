af31bbf174652a00891c8986bb49e45f492901a8
Jullie <blema90.jl@gmail.com>
2018-05-20 12:31:25 -0400

    committing company form sign up and added nav bar welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link, Route, Switch} from "react-router-dom";
 import Home from "./signup/Home";
-import NewCompany from "./signup/NewCompany";
+import CompanySignUpForm from "./signup/CompanySignUpForm";
 
 class App extends React.Component {
   constructor (){
@@ -14,18 +14,27 @@ class App extends React.Component {
 
     return (
     <div>
-      <nav>
-      <Link to="/">Home</Link>
-      {" "}
-      <Link to="/companysignup">Sign Up</Link>
-      </nav>
-      
-      <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/companysignup" component={NewCompany}/>
-      </Switch>
-  </div>
+      <div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div class="bg-dark p-4">
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/companysignup">Sign Up</Link>
+          </div>
+        </div>
 
+  <nav class="navbar navbar-dark bg-dark">
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+   </nav>
+  
+</div>
+    <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/companysignup" component={CompanySignUpForm}/>
+    </Switch>
+</div>
    
     )
   }
