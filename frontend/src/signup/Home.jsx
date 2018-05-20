85533e783f4255689e2e1594fe23b1a982e55ada
Jullie <blema90.jl@gmail.com>
2018-05-19 21:48:44 -0400

    committing 1st front end Imanage home page

---
+++ frontend/src/signup/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+const Home = () => (
+    <div>
+        <h1> Welcome To Imanage </h1>
+        <p>IMANAGE is a simple app to clock in and out that helps small business owners manage by allowing their employees to report their time when on the field. </p>
+        <button disabled={''} type="button" class="btn btn-primary">Sign Up</button>
+
+    </div>
+)
+
+
+
+
+
+export default Home;
